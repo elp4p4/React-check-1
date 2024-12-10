ca01edd39a9389c338310bd1e71507f3b6523fed
Ahmed <brazfu145@gmail.com>
2024-12-10 10:42:43 +0100

    supp

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import product from '../product';
 
 const Name = () => {
-    return <h3 className="text-primary mt-3">{product.name}</h3>; // Bootstrap class
+    return <h3 className="text-primary mt-3">{product.name}</h3>;
 };
 
 export default Name;
