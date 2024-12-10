7fa2a3d7ce33ec9c058da1cca32f4f6d9db82d13
Ahmed <brazfu145@gmail.com>
2024-12-10 10:40:47 +0100

    width

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     border: "none",
     borderRadius: "10px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    width: "300px",
+    width: "450px",
     margin: "20px auto",
   };
 
