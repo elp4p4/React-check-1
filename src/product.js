393a4fd95dccb75e68c9206c50819b21830d70ba
elp4p4 <brazfu145@gmail.com>
2024-12-11 21:11:54 +0100

    added comments

---
+++ src/product.js
@@ -0,0 +1,8 @@
+const product = {
+    name: "AirPods Pro 2",
+    price: "$199.99",
+    description: "APPLE Airpods Pro 2ème Génération Avec Boitier De Charge MagSafe - Technologie de connectivité: Bluetooth 5.3 - Autonomie: Jusqu'à 6 heures d'autonomie sur une seule charge, Jusqu'à 30 h de lecture avec boitier , Conversation continue jusqu'à 24 heures - Suppression active du bruit - Microphones à double formation de faisceau - Capteur de détection de peau, Accéléromètre à détection de mouvement, Accéléromètre à détection de parole - Audio spatial personnalisé avec suivi dynamique de la tête",
+    image: "https://mk-media.mytek.tn/media/catalog/product/cache/8be3f98b14227a82112b46963246dfe1/e/c/ecouteurs-sans-fil-apple-airpods-pro-2-eme-generation-blanc.jpg"
+};
+
+export default product;
